1df2cfd7a773cd8b63bd3759ce8f0e18803c0fcf
sillyTroll <saradalfalk@gmail.com>
2017-05-26 14:22:28 +0200

    Revert "Added Js prefix on id"
    This reverts commit 9e4344981c16d40ddd86805714ba5f20b641430a.

--- scripts/addTrack.js
+++ scripts/addTrack.js
@@ -5,7 +5,7 @@ function addTrack (){
     var contentful = require('contentful-management')
     var client = contentful.createClient({
         // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-        accessToken: 'b60f393ec836a43747cb5a238cdc49e379361c7d7a0a96012191fb3745e2532b'
+        accessToken: ''
 
     });
 
